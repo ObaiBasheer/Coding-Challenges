345bf16b384299da98824b78747e40d7f77c8d80
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2023-12-13 09:27:49 +0300

    Add Bittorrent challenges

---
+++ bittorrent/src/main.rs
@@ -0,0 +1,32 @@
+use serde_json;
+use std::env;
+
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    //if encoded_Value strats with  adigit it's a number
+    if let Some(rest) = encoded_value.strip_prefix('i') {
+        if let Some((digits, _)) = rest.split_once('e') {
+            if let Ok(n) = digits.parse::<i64>() {
+                return n.into();
+            }
+        }
+    }
+    else if let Some((len, rest)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
+    }
+    panic!("Unhandled encoded value : {}", encoded_value);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+
+        println!("{}", serde_json::to_string_pretty(&decoded_value).unwrap())
+    }
+}
